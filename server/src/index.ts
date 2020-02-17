e19d7b66108d6c2a3d52593cd9d5e02e9763a961
christine-aqui <aqui.christine@gmail.com>
2020-02-17 15:41:45 -0500

    lesson_1.11 - get and post express routes tested using curl

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,29 @@
 import express from 'express';
+import { listings } from './listings';
+import bodyParser from 'body-parser';
 const app = express();
 const PORT: number = 9000;
 
-app.get('/', (req, res) => {
+app.use(bodyParser.json());
+
+app.get('/', (_req, res) => {
 	res.send('hello world');
+});
+
+app.get('/listings', (_req, res) => {
+	return res.send(listings);
+});
+
+app.post('/delete-listing', (req, res) => {
+	const id: string = req.body.id;
+
+	for (let i = 0; i < listings.length; i++) {
+		if (listings[i].id === id) {
+			return res.send(listings.splice(i, 1)); // delete and return delete item
+		}
+	}
+
+	return res.send('fails to delete listing');
 });
 
 app.listen(PORT, () => {
