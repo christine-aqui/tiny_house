75d044f8a2a942a9cf096aaa11f6918cf9ebf70e
christine-aqui <aqui.christine@gmail.com>
2020-02-19 22:28:11 -0500

    lesson_6.7 - gave client ability to connect to graphql server

---
+++ client/src/sections/Listings/Listings.tsx
@@ -0,0 +1,40 @@
+import React, { FunctionComponent } from 'react';
+import { server } from '../../lib/api';
+
+// graphQL query
+const LISTINGS = `
+	query {
+		listings {
+			id
+			title
+			image
+			address
+			price
+			numOfGuests
+			numOfBeds
+			rating
+		}
+	}
+`;
+
+interface Props {
+	title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+	const fetchListings = async () => {
+		const { data } = await server.fetch({ query: LISTINGS });
+		console.log(data);
+	};
+
+	return (
+		<div>
+			<h2>{title} </h2>
+			<button onClick={fetchListings}>Query Listings!</button>
+		</div>
+	);
+};
+
+// export const Listings2: FunctionComponent<Props> = ({ title, children }) => {
+// 	return <h2>{title} </h2>;
+// };
